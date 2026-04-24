114a04683b121077781bef08955f65c60d34bc9e
amatubay16-ai <amatubay16@gmail.com>
2026-04-24 09:11:14 +0800

    Add files via upload

---
+++ student.js
@@ -0,0 +1,9 @@
+const name = "Almira Matubay";
+const course = "BSIT";
+const year = "3rd Year";
+const language = "JavaScript";
+
+console.log("Name:", name);
+console.log("Course:", course);
+console.log("Year Level:", year);
+console.log("Favorite Language:", language);
