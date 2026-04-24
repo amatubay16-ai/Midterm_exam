f5d926db3d86574260964097b9926c1b29f7dbf8
amatubay16-ai <amatubay16@gmail.com>
2026-04-24 09:23:51 +0800

    Delete student.js

--- student.js
+++
@@ -1,9 +0,0 @@
-const name = "Almira Matubay";
-const course = "BSIT";
-const year = "3rd Year";
-const language = "JavaScript";
-
-console.log("Name:", name);
-console.log("Course:", course);
-console.log("Year Level:", year);
-console.log("Favorite Language:", language);
